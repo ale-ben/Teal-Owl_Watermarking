dff8c3088989d9b6b2a7912838d1c4499f7e186c
Alessandro Benetton <aleben98@gmail.com>
2023-02-13 11:24:25 +0100

    [master] Convert from class to module

--- watermarkingTools.ts
+++ watermarkingTools.ts
@@ -1,7 +1,7 @@
-export class WatermarkingTools {
-	public static readonly NPC: string = "\u00a7"; // <>;
+export module WatermarkingTools {
+	export const NPC: string = "\u00a7"; // <>;
 
-	public static readonly TableCharacters: {
+	export const TableCharacters: {
 		original: string[];
 		homoglyph: string[];
 	} = {
@@ -43,7 +43,7 @@ export class WatermarkingTools {
 		]
 	};
 
-	public static readonly TableSpaces: string[] = [
+	export const TableSpaces: string[] = [
 		"\u0020",
 		"\u2000",
 		"\u2004",
@@ -54,9 +54,9 @@ export class WatermarkingTools {
 		"\u205f"
 	];
 
-	private static getSpaceCode(space : string): string {
+	function getSpaceCode(space : string): string {
 		// Find the element in the array
-		let index = this.TableSpaces.indexOf(space);
+		let index = TableSpaces.indexOf(space);
 
 		// If not found, return an empty string
 		if (index == -1) {
@@ -70,7 +70,7 @@ export class WatermarkingTools {
 		return binary;
 	}
 
-	private static getSpaceFromCode(code : string): string {
+	function getSpaceFromCode(code : string): string {
 		// Convert the binary to an integer
 		let index = parseInt(code, 2);
 
@@ -80,16 +80,16 @@ export class WatermarkingTools {
 			return "";
 		}
 		
-		return this.TableSpaces[index];
+		return TableSpaces[index];
 	}
 
-	private static getCharacterWithHomoglyph(character : string, value : string): string {
+	function getCharacterWithHomoglyph(character : string, value : string): string {
 		let charSet: string[] = value == "1"
-			? this.TableCharacters.homoglyph
-			: this.TableCharacters.original;
+			? TableCharacters.homoglyph
+			: TableCharacters.original;
 
 		// Find the element in the array
-		let index = this.TableCharacters.original.indexOf(character);
+		let index = TableCharacters.original.indexOf(character);
 
 		// If not found, return an empty string
 		if (index == -1) {
@@ -100,17 +100,17 @@ export class WatermarkingTools {
 		return charSet[index];
 	}
 
-	private static getCodeFromCharacter(character : string): string {
-		if (this.TableCharacters.original.indexOf(character) != -1) 
+	function getCodeFromCharacter(character : string): string {
+		if (TableCharacters.original.indexOf(character) != -1) 
 			return "0";
-		if (this.TableCharacters.homoglyph.indexOf(character) != -1) 
+		if (TableCharacters.homoglyph.indexOf(character) != -1) 
 			return "1";
 		return "";
 	}
 
-	public static encodeText(text : string, binaryCode : string): string {
+	export function encodeText(text : string, binaryCode : string): string {
 		// Initializa output string with the NPC
-		var outText = this.NPC;
+		var outText = NPC;
 
 		// Iterator index for binary code
 		var binIter = 0;
@@ -119,10 +119,10 @@ export class WatermarkingTools {
 
 		// Iterate over the text
 		for (const c of text) {
-			if (this.TableCharacters.original.includes(c)) {
-				outText = outText.concat(this.getCharacterWithHomoglyph(c, binaryCode.charAt(binIter)));
+			if (TableCharacters.original.includes(c)) {
+				outText = outText.concat(getCharacterWithHomoglyph(c, binaryCode.charAt(binIter)));
 				binIter += 1;
-			} else if (this.TableSpaces.includes(c)) {
+			} else if (TableSpaces.includes(c)) {
 				// Get the binary code for the space (composed of 3 bits with eventual 0 padding)
 				var bitStr = "";
 				for (let i = binIter; i < binIter + 3; i++) {
@@ -132,7 +132,7 @@ export class WatermarkingTools {
 						bitStr = bitStr.concat("0");
 					}
 				}
-				outText = outText.concat(this.getSpaceFromCode(bitStr));
+				outText = outText.concat(getSpaceFromCode(bitStr));
 				binIter += 3;
 			} else {
 				outText = outText.concat(c);
@@ -140,17 +140,17 @@ export class WatermarkingTools {
 
 			// If the binary code is over, add the NPC and restart from 0
 			if (binIter >= binLen) {
-				outText = outText.concat(this.NPC);
+				outText = outText.concat(NPC);
 				binIter = 0;
 			}
 		}
 
-		return outText.concat(this.NPC).replaceAll(this.NPC + this.NPC, this.NPC);
+		return outText.concat(NPC).replaceAll(NPC + NPC, NPC);
 	}
 
 	// Decode a single paragraph
-	public static decodeParagraph(text : string): string[] {
-		const paragraphs = text.split(this.NPC);
+	export function decodeParagraph(text : string): string[] {
+		const paragraphs = text.split(NPC);
 
 		  const decodedParagraphs = paragraphs.reduce(function(result : string[], paragraph: string) {
 			
@@ -170,19 +170,19 @@ export class WatermarkingTools {
 	}
 
 	// Split text based on NPC and call decodeParagraph on all paragraphs
-	public static decodeText(text : string): string {
+	export function decodeText(text : string): string {
 		
 		// Initialize the output string
 		var outCode = "";
 
 		// Iterate over the text
 		for (const c of text) {
-			if (this.TableCharacters.original.includes(c)) {
+			if (TableCharacters.original.includes(c)) {
 				outCode = outCode.concat("0");
-			} else if (this.TableCharacters.homoglyph.includes(c)) {
+			} else if (TableCharacters.homoglyph.includes(c)) {
 				outCode = outCode.concat("1");
-			} else if (this.TableSpaces.includes(c)) {
-				outCode = outCode.concat(this.getSpaceCode(c));
+			} else if (TableSpaces.includes(c)) {
+				outCode = outCode.concat(getSpaceCode(c));
 			}
 
 		}
@@ -190,10 +190,3 @@ export class WatermarkingTools {
 		return outCode;
 	}
 }
-
-// const payload = "0101010001100101011110000111010000100000011101110110000101110100011001010111001001101101011000010111001001101011011010010110111001100111001000000110100101110011001000000110000101110111011001010111001101101111011011010110010100100001";
-// const originalText = "The ⅼast decaⅾes are charaⅽterⅰzeⅾ by the easy avaiⅼability of miⅼⅼⅰons upon miⅼlⅰons of dⅰgⅰtal ⅽontents that meet seⅴeraⅼ kinⅾ of users’ neeⅾs both in professⅰonal aⅽtⅰvⅰties anⅾ soⅽⅰal";
-					
-// let dec = WatermarkingTools.decodeText(originalText);
-// console.log(dec);
-// console.log(dec == payload);
